eaab32e4e38ac3bb7c58bc75772e05c259ed3d74
rezansrv <rnaserivand@gmail.com>
2023-10-21 17:12:49 +0330

    hello

---
+++ script.js
@@ -0,0 +1,26 @@
+// script.js
+function openPopup(imageUrl) {
+    var popup = document.getElementById("popup");
+    var popupImage = document.getElementById("popup-image");
+    var downloadLink = document.getElementById("download-link");
+  
+    popup.style.display = "block";
+    popupImage.src = imageUrl;
+    downloadLink.href = imageUrl;
+  }
+  
+  function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    var imageElements = document.querySelectorAll('.gallery img');
+  
+    imageElements.forEach(function(element) {
+      element.addEventListener('click', function() {
+        var imageSrc = this.getAttribute('data-image-src');
+        openPopup(imageSrc);
+      });
+    });
+  });
